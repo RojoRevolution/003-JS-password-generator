07d54062407c6e05d57da47978eb13c7b544e171
David Rojo <rojo@rojorevolution.com>
2020-12-24 20:36:30 -0600

    Added notes to test Javascript file explainging logic

--- Develop/tests/test.js
+++ Develop/tests/test.js
@@ -1,25 +1,24 @@
 //////////// Brainstorming Code --- Will need to reorganize////////////////
 
-
-
-
 function randomGen() {
     // Prompt for length
     var lengthPrompt = prompt("Choose the length of you password (between 8-128):");
     // need to validate length
     console.log(lengthPrompt);
+    // Variable Stores the user length
     var passLength = lengthPrompt;
     //Prompt for Uppercase
-    var inclUpperCase = confirm("Would you like to include Uppercase Characters?")
-    var inclNum = confirm("Would you like to include Numbers?")
-    var inclSpecial = confirm("Would you like to include special characters?")
+    var inclUpperCase = confirm("Would you like to include Uppercase Characters?");
+    //Prompt for including numbers
+    var inclNum = confirm("Would you like to include Numbers?");
+    //Prompt for including Special Characters
+    var inclSpecial = confirm("Would you like to include special characters?");
 
-    //Prompt for numbers
-    //Prompt for Special Characters
     var char = 'abcdefghijklmnopqrstuvwxyz';
     var charUpper = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var charUpperNum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
     var charUpperNumSpecial = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*=-_';
+    //empty variable that will store the password
     var passWord = "";
 
 
@@ -28,7 +27,7 @@ function randomGen() {
         for (var i = 0; i < passLength; i++) {
             var randGenerate = Math.floor(Math.random() * char.length);
             var match = char[randGenerate];
-            //Pushing to a string
+            //Push var match to the passWord variable to a string
             passWord += match;
         };
         console.log(passWord);
@@ -37,7 +36,6 @@ function randomGen() {
         for (var i = 0; i < passLength; i++) {
             var randGenerate = Math.floor(Math.random() * charUpper.length);
             var match = charUpper[randGenerate];
-            //Pushing to a string
             passWord += match;
         };
         console.log(passWord);
@@ -46,7 +44,6 @@ function randomGen() {
         for (var i = 0; i < passLength; i++) {
             var randGenerate = Math.floor(Math.random() * charUpperNum.length);
             var match = charUpperNum[randGenerate];
-            //Pushing to a string
             passWord += match;
         };
         console.log(passWord);
@@ -55,7 +52,6 @@ function randomGen() {
         for (var i = 0; i < passLength; i++) {
             var randGenerate = Math.floor(Math.random() * charUpperNumSpecial.length);
             var match = charUpperNumSpecial[randGenerate];
-            //Pushing to a string
             passWord += match;
         };
         console.log(passWord);
