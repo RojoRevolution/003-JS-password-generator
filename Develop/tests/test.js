1f7b6bee18681948677ff84638b19dde43acb8dd
David Rojo <rojo@rojorevolution.com>
2020-12-24 20:19:38 -0600

    added confirms for other options, added variables for uppercade, numbers and special characters

--- Develop/tests/test.js
+++ Develop/tests/test.js
@@ -10,20 +10,24 @@ function randomGen() {
     console.log(lengthPrompt);
     var passLength = lengthPrompt;
     //Prompt for Uppercase
-    var upperCase = confirm("Would you like to include Uppercase Characters?")
+    var inclUpperCase = confirm("Would you like to include Uppercase Characters?")
+    var inclNum = confirm("Would you like to include Numbers?")
+    var inclSpecial = confirm("Would you like to include special characters?")
+
     //Prompt for numbers
     //Prompt for Special Characters
     var char = 'abcdefghijklmnopqrstuvwxyz';
-    var num = '0123456789';
-    var sym = '!@#$%^&*=-_';
+    var charUpper = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charUpperNum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    var charUpperNumSpecial = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*=-_';
     var passWord = "";
 
 
     // var num = Math.floor(Math.random() * ((20 - 10) + 1) + 10;
 
     for (var i = 0; i < passLength; i++) {
-        var randGenerate = Math.floor(Math.random() * char.length);
-        var match = char[randGenerate];
+        var randGenerate = Math.floor(Math.random() * charUpperNumSpecial.length);
+        var match = charUpperNumSpecial[randGenerate];
         //Pushing to a string
         passWord += match;
     };
