dac455ca24c974cfabbd124d48e02ec55082e48a
David Rojo <rojo@rojorevolution.com>
2020-12-24 23:50:36 -0600

    created fucntion randomGen2 which includes a restructured interior

--- Develop/tests/test.js
+++ Develop/tests/test.js
@@ -56,14 +56,53 @@ function randomGen() {
         };
         console.log(passWord);
     };
-}
+};
 
 
-randomGen();
+//randomGen();
 
-// let show = myShows[Math.floor(Math.random() * myShows.length)];
+/////////// Test-002
+
+function randomGen2() {
+    // Prompt for length
+    var lengthPrompt = prompt("Choose the length of you password (between 8-128):");
+    // need to validate length
+    console.log(lengthPrompt);
+    // Variable Stores the user length
+    var passLength = lengthPrompt;
+    //Prompt for Uppercase
+    var inclUpperCase = confirm("Would you like to include Uppercase Characters?");
+    //Prompt for including numbers
+    var inclNum = confirm("Would you like to include Numbers?");
+    //Prompt for including Special Characters
+    var inclSpecial = confirm("Would you like to include special characters?");
+
+    // var char = 'abcdefghijklmnopqrstuvwxyz';
+    var charUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charNum = '1234567890';
+    var charSpecial = '!@#$%^&*=-_';
+    //empty variable that will store the password
+    var requirements = "abcdefghijklmnopqrstuvwxyz";
 
 
+    if (inclUpperCase === true) { requirements += charUpper; }
+    //include false statements so that it saves the results?
+    if (inclNum === true) { requirements += charNum; }
+    if (inclSpecial === true) { requirements += charSpecial; }
+
+    // var num = Math.floor(Math.random() * ((20 - 10) + 1) + 10;
+    for (var i = 0; i < passLength; i++) {
+        var passWord = "";
+        var randGenerate = Math.floor(Math.random() * requirements.length);
+        var match = requirements[randGenerate];
+        //Push var match to the passWord variable to a string
+        passWord += match;
+    };
+    console.log(passWord);
+};
+
+
+randomGen(2);
 
 
 
